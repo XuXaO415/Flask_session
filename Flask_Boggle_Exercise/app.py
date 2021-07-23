6ac24d5cc9288aa595f0f2c2579dacea798d676c
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-07-22 20:07:43 -0700

    Added @app.route to this exercise

--- Flask_Boggle_Exercise/app.py
+++ Flask_Boggle_Exercise/app.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, render_template, session
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+
+
+# @app.route("/")
