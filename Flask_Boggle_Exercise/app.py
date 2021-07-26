dcef75fb72f7cad6b12b4879f5a72d7c4b6524d3
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-07-25 19:23:58 -0700

    Added index.html, templates and basic startup for test.app

--- Flask_Boggle_Exercise/app.py
+++ Flask_Boggle_Exercise/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, jsonify
 from boggle import Boggle
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "abc123"
+app.config["SECRET_KEY"] = "SHHHHHHHHHHH SEEKRIT"
 
 
-# @app.route("/")
+@app.route("/")
+def show_homepage():
+    """Shows homepage with board"""
+    board = boggle_game.make_board()
+    session["board"] = board
+    return render_template("index.html", board=board)
--- Flask_Boggle_Exercise/test.py
+++ Flask_Boggle_Exercise/test.py
@@ -1,11 +1,28 @@
 from unittest import TestCase
 from app import app
-from flask import session
+from flask import Flask, session
+# from flask import session
 from boggle import Boggle
+
+# Make Flask errors be real errors, not HTML pages with error info
+app.config["Testing"] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
-    # def test_case(self):
+    def setUp(self):
+        """Set up test"""
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session["count"] = 999
+                res = client.get("/")
+                self.assertEqual(res.status_code, 200)
+                self.assertEqual(session['count'], 1000)
+
+    # def tearDown(self):
+
+    # def test_homepage(self):
+        # def test_1(self):
